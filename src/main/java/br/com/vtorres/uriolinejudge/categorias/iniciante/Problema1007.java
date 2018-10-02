3c980c977dad789cdb8c0b2940f6bb77b11ff768
Victor Torres <victor.h.c.torres@gmail.com>
2018-10-01 22:23:10 -0300

    implementacao do problema 1007

--- src/main/java/br/com/vtorres/uriolinejudge/categorias/iniciante/Problema1007.java
+++ src/main/java/br/com/vtorres/uriolinejudge/categorias/iniciante/Problema1007.java
@@ -6,33 +6,33 @@ public class Problema1007 {
 
     public static void main(String[] args){
 
-    	double[] entrada = entrada();
-        double retorno = calcularValor(entrada);
+    	int[] entrada = entrada();
+        int retorno = calcularValor(entrada);
         saida(retorno);
     }
 
-    private static void saida(double retorno) {
-    	System.out.printf("MEDIA = %.1f\n", retorno);
+    private static void saida(int retorno) {
+    	System.out.printf("DIFERENCA = %d\n", retorno);
 	}
 
-	private static double[] entrada() {
+	private static int[] entrada() {
         Scanner teclado = new Scanner(System.in);
-        double a = converterString2Double(teclado.next());
-        double b = converterString2Double(teclado.next());
-        double c = converterString2Double(teclado.next());
+        int a = converterString2Double(teclado.next());
+        int b = converterString2Double(teclado.next());
+        int c = converterString2Double(teclado.next());
+        int d = converterString2Double(teclado.next());
         
-        double[] entrada = {a, b, c};
+        int[] entrada = {a, b, c, d};
         return entrada;
 	}
 
-	private static double converterString2Double(String valorIn) {
-		double valorInConvertido = Double.valueOf(valorIn);
-    	return ((valorInConvertido < 0.0 || valorInConvertido > 10.0) ? 0.0 : valorInConvertido);
+	private static int converterString2Double(String valorIn) {
+		int valorInConvertido = Integer.valueOf(valorIn);
+    	return valorInConvertido;
 	}
 
-	private static double calcularValor(double[] entrada) {
-        double[] peso = {2.0, 3.0, 5.0};
-        double x = ((entrada[0] * peso[0]) + (entrada[1] * peso[1]) + + (entrada[2] * peso[2])) / (peso[0] + peso[1] + peso[2]);
+	private static int calcularValor(int[] entrada) {
+        int x = (entrada[0] * entrada[1] - entrada[2] * entrada[3]);
         
         return x;
     }
